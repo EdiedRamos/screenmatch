4ae5d38a58b34a6c93e8144dcc0140bc4265b0d8
EdiedRamos <edieisa22@gmail.com>
2024-04-27 22:41:45 -0500

    chore: add json ignore configuration

--- src/main/java/com/aluracursos/screenmatch/model/SerieData.java
+++ src/main/java/com/aluracursos/screenmatch/model/SerieData.java
@@ -1,7 +1,9 @@
 package com.aluracursos.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record SerieData(
         @JsonAlias("Title") String title,
         @JsonAlias("totalSeasons") Integer totalSeasons,
