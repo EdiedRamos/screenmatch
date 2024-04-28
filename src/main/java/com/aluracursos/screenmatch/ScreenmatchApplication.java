2751bb8bdd4dd4b4335d8fdf0525309fc7b453b9
EdiedRamos <edieisa22@gmail.com>
2024-04-27 22:46:20 -0500

    chore: check deserialized data

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.model.SerieData;
 import com.aluracursos.screenmatch.service.APIConsumer;
+import com.aluracursos.screenmatch.service.DataConverter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,8 +17,12 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var api = new APIConsumer();
-//		var mrRobot = api.getData("http://www.omdbapi.com/?apikey=9a9b70da&t=mr+robot");
-		var fakeUsers = api.getData("https://jsonplaceholder.typicode.com/users");
-		System.out.println(fakeUsers);
+		var mrRobot = api.getData("http://www.omdbapi.com/?apikey=9a9b70da&t=mr+robot");
+//		var fakeUsers = api.getData("https://jsonplaceholder.typicode.com/users");
+//		System.out.println(fakeUsers);
+
+		var converter = new DataConverter();
+		var data = converter.getData(mrRobot, SerieData.class);
+		System.out.println(data);
 	}
 }
