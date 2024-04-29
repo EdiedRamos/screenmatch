5b3015c0cafa4fbf05a46d465f819ffe788ba9a3
EdiedRamos <edieisa22@gmail.com>
2024-04-28 22:12:20 -0500

    chore: add episodedata model

---
+++ src/main/java/com/aluracursos/screenmatch/model/EpisodeData.java
@@ -0,0 +1,16 @@
+package com.aluracursos.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record EpisodeData(
+        @JsonAlias("Title")
+        String title,
+        @JsonAlias("Year")
+        Integer year,
+        @JsonAlias("Runtime")
+        String duration,
+        @JsonAlias("imdbRating")
+        Float rating
+) {}
